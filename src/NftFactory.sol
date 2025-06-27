cbadc9be1877b6793e9a9814c250f7622b227cb0
andres busta <andresbusta4444@gmail.com>
2025-06-27 16:28:14 -0400

    add factory proxy pattern

---
+++ src/NftFactory.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.30;
+import {SimpleNft} from "./SimpleNft.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
+contract NftFactory is Ownable {
+    address public implementation;
+    address[] public allNFTs;
+
+    event NFTCreated(address indexed proxyAddress);
+
+    constructor() Ownable(msg.sender) {
+        implementation = address(new SimpleNft(msg.sender));
+    }
+
+    function createNFT() external onlyOwner returns (address) {
+        ERC1967Proxy proxy = new ERC1967Proxy(
+            implementation,
+            abi.encodeWithSelector(
+                SimpleNft(implementation).safeMint.selector,
+                20
+            )
+        );
+
+        allNFTs.push(address(proxy));
+        emit NFTCreated(address(proxy));
+        return address(proxy);
+    }
+
+    function getAllNFTs() external view returns (address[] memory) {
+        return allNFTs;
+    }
+}
